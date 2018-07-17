b7bf4576f5babee458ce46b4ea335987e48a2ef8
Irvan <Mr.vero@dr.com>
2018-07-17 21:11:54 +0700

    Delete test_helper.rb

--- test_helper.rb
+++
@@ -1,8 +0,0 @@
-require "rack/test"
-require "test/unit"
-
-# Always use local Commuter
-this_dir = File.join(File.dirname(__FILE__), "..")
-$LOAD_PATH.unshift File.expand_path(this_dir)
-
-require "commuter"
